1268ec7d0984bad78b2018f38ca1c9cc5180b4f3
Sebastian Roll <sebastianroll84@gmail.com>
2021-12-14 18:14:59 +0100

    Complete part2 of day14

--- src/day14.rs
+++ src/day14.rs
@@ -9,42 +9,96 @@ pub fn part2(path: &str) -> usize {
     solve(path, 40)
 }
 
-fn solve(path: &str, iteration_steps: usize) -> usize {
+fn solve(path: &str, max_step: usize) -> usize {
     let contents = fs::read_to_string(path).unwrap();
 
-    let mut current = contents.lines().nth(0).unwrap().to_owned();
+    let current = contents.lines().nth(0).unwrap().to_owned();
 
     let mut pair_rules = HashMap::new();
     for line in contents.lines().skip(2) {
         let mut iter = line.split(" -> ");
-        pair_rules.insert(iter.next().unwrap(), iter.next().unwrap());
+        pair_rules.insert(
+            iter.next().unwrap(),
+            iter.next().unwrap().chars().nth(0).unwrap(),
+        );
     }
 
-    for step in 1..=iteration_steps {
-        eprintln!("step = {:?}", step);
-        current = iteration(&current[..], &pair_rules);
-    }
+    // First we find the 20-step-forward frequencies for every mapping key
+    let mut forward_twenty = HashMap::new();
+    pair_rules.keys().for_each(|key| {
+        let mut freqs_forward = HashMap::new();
+        let mut iter = key.chars();
+        let a = iter.next().unwrap();
+        let b = iter.next().unwrap();
+        iteration(a, b, &pair_rules, 0, max_step / 2, &mut freqs_forward, None);
+        forward_twenty.insert(*key, freqs_forward);
+    });
 
+    // Then we find 20-step-forward frequencies for the char pairs in our codeword (top of the input file)
     let mut freqs = HashMap::new();
-    current
-        .chars()
-        .for_each(|c| *freqs.entry(c).or_insert(0) += 1);
+    current.as_bytes().windows(2).for_each(|w| {
+        iteration(
+            w[0] as char,
+            w[1] as char,
+            &pair_rules,
+            0,
+            max_step / 2,
+            &mut freqs,
+            Some(&forward_twenty),
+        );
+    });
+
+    // Add the original characters
+    for c in current.chars() {
+        *freqs.entry(c).or_insert(0) += 1;
+    }
+
+    // Final calculation: returns highest char count minus lowest char count
     let mut counts: Vec<usize> = freqs.values().cloned().collect();
     counts.sort();
     counts[counts.len() - 1] - counts[0]
 }
 
-fn iteration(current: &str, pair_rules: &HashMap<&str, &str>) -> String {
-    let new = current
-        .as_bytes()
-        .windows(2)
-        .map(|w| {
-            let s = format!("{}{}", w[0] as char, w[1] as char); //[w[0] as char, w[1] as char].iter().collect::<String>();
+fn iteration(
+    a: char,
+    b: char,
+    pair_rules: &HashMap<&str, char>,
+    step: usize,
+    max_step: usize,
+    freqs: &mut HashMap<char, usize>,
+    forward_twenty: Option<&HashMap<&str, HashMap<char, usize>>>,
+) {
+    let comb = format!("{}{}", a, b);
 
-            let mid = pair_rules.get(&s.as_str()).unwrap();
-            format!("{}{}", w[0] as char, mid)
-        })
-        .collect::<String>();
-    let last_char: char = current.chars().rev().nth(0).unwrap();
-    format!("{}{}", new, last_char)
+    if step >= max_step {
+        if let Some(for_twenty) = forward_twenty {
+            let ft = &for_twenty[&comb.as_str()];
+            for (c, count) in ft.iter() {
+                *freqs.entry(*c).or_insert(0) += count;
+            }
+        }
+        return;
+    }
+
+    let mid = pair_rules.get(&comb.as_str()).unwrap();
+    *freqs.entry(*mid).or_insert(0) += 1;
+
+    iteration(
+        a,
+        *mid,
+        pair_rules,
+        step + 1,
+        max_step,
+        freqs,
+        forward_twenty,
+    );
+    iteration(
+        *mid,
+        b,
+        pair_rules,
+        step + 1,
+        max_step,
+        freqs,
+        forward_twenty,
+    );
 }
